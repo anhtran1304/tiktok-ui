37c20061610404abb85247ca179113040c5d2a64
anhtran1304 <theanhtran2000@gmail.com>
2022-11-19 16:15:34 +0700

    112. Áp dụng PropTypes vào dự án

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 
@@ -48,10 +49,11 @@ function Menu({
             hideOnClick={hideOnClick}
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx('menu-popper')}>
+                    <PopperWrapper
+                     className={cx('menu-popper')}>
                         {history.length > 1 && (
                             <Header
-                                title="Language"
+                                title={current.title}
                                 onBack={() => {
                                     setHistory((prev) =>
                                         prev.slice(0, prev.length - 1),
@@ -68,6 +70,13 @@ function Menu({
             {children}
         </Tippy>
     );
+}
+
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func,
 }
 
 export default Menu;
